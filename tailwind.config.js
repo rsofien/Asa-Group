6999a60890b90184ba6bf0472dfe87883b1acf86
rsofien <108265058+rsofien@users.noreply.github.com>
2022-10-03 18:15:17 +0100

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px'
+    },
+    extend: {
+      colors: {
+        brightRed: 'hsl(12, 88%, 59%)',
+        brightRedLight: 'hsl(12, 88%, 69%)',
+        brightRedSupLight: 'hsl(12, 88%, 95%)',
+        darkBlue: 'hsl(228, 39%, 23%)',
+        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        veryDarkBlue: 'hsl(233, 12%, 13%)',
+        veryPaleRed: 'hsl(13, 100%, 96%)',
+        veryLightGray: 'hsl(0, 0%, 98%)',
+      },
+      backgroundImage: {
+        'comite-1': "url('../img/comite-executif-1.jpg')",
+        'comite-2': "url('../img/comite-executif-2.jpg')",
+        'comite-3': "url('../img/comite-executif-3.jpg')"
+      },
+    },
+  },
+  plugins: [],
+}
